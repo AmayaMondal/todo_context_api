59939f0350034ea38d3ca69b47e05b74537a8288
AmayaMondal <amaya27mondal@gmail.com>
2025-08-20 02:51:04 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,71 @@
+import { useEffect, useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import { TodoProvider } from "./contexts";
+
+function App() {
+  const [todos, settodos] = useState([]); //here in todos all todo are there
+
+  const addTodo = (todo) => {
+    settodos((prev) => [...prev, { id: Date.now(), ...todo }]);
+  };
+
+  const updatedTodo = (id, todo) => {
+    settodos((prev) =>
+      prev.map((prevTodo) => (prevTodo.id == id ? todo : prevTodo))
+    );
+  };
+
+  const deleteTodo = (id) => {
+    settodos((prev) => prev.filter((todo) => todo.id !== id));
+  };
+
+  const toggleComplete = (id) => {
+    settodos((prev) =>
+      prev.map((todo) =>
+        todo.id === id ? { ...todos, completed: !todo.completed } : todo
+      )
+    );
+  };
+
+  //this getitem and set item is used to access local storage that is the browser's storage
+  useEffect(() => {
+    const todos = JSON.parse(localStorage.getItem(todos));
+    if (todos && todos.length > 0) {
+      settodos(todos);
+    } //this line is for to check whether todos are there in local storage or not ..if it is not checked then the application will crash
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem("todos", JSON.stringify(todos));
+  }, [todos]);
+
+  return (
+    <TodoProvider
+      value={{ todos, addTodo, updatedTodo, deleteTodo, toggleComplete }}
+    >
+      <div className="bg-[#172842] min-h-screen py-8">
+        <div className="w-full max-w-2xl mx-auto shadow-md rounded-lg px-4 py-3 text-white">
+          <h1 className="text-2xl font-bold text-center mb-8 mt-2">
+            Manage Your Todos
+          </h1>
+          <div className="mb-4">
+            {/* Todo form goes here */}
+            <TextForm />
+          </div>
+          <div className="flex flex-wrap gap-y-3">
+            {/*Loop and Add TodoItem here */}
+            {todos.map((todo) => (
+              <div key={todo.id} className="w-full">
+                <TextItem todo={todo} />
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+    </TodoProvider>
+  );
+}
+
+export default App;
---
+++ src/components/index.js
@@ -0,0 +1,3 @@
+import TodoForm from "./TextForm";
+import TodoItem from "./TextItem";
+export { TodoForm, TodoItem, TodoContext } from "./index";
